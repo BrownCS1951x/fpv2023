3acf68b4c3fa96b2d6a50f2ba09e9b74d018c54a
Rob Lewis <rob.y.lewis@gmail.com>
2023-09-08 10:53:23 -0400

    globally disable unused argument linter and autoimplicits

--- lakefile.lean
+++ lakefile.lean
@@ -2,12 +2,17 @@ import Lake
 
 open Lake DSL
 
-package love
+def extraArgs := #["-DautoImplicit=false", "-Dlinter.unusedVariables=false"]
+
+package love {
+  moreServerArgs := extraArgs
+}
 
 @[default_target]
 lean_lib LoVe {
   roots := #[`LoVe]
   globs := #[Glob.submodules `LoVe]
+  moreLeanArgs := extraArgs 
 }
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "ba80034619884735df10ebb449b4fc2a44b2c3e7"
