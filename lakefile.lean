8a10f708e29ea001e1499b34a937eb2864a0f9f1
Rob Lewis <rob.y.lewis@gmail.com>
2023-09-25 14:05:54 -0400

    update autograder version

--- lakefile.lean
+++ lakefile.lean
@@ -16,5 +16,5 @@ lean_lib LoVe {
 }
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "ba80034619884735df10ebb449b4fc2a44b2c3e7"
-require autograder from git "https://github.com/robertylewis/lean4-autograder-main" @ "d1fcb4ce126aa074f5e35514c0754d43486b7b22"
+require autograder from git "https://github.com/robertylewis/lean4-autograder-main" @ "306fd844e8bdcd6c7440ed9b7ae7f16ca86c10cd"
   
